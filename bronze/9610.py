9e5535bbc71665934815377700e3c507023125e2
[KimJungIn] <[rlawjddla0203@gmail.com]>
2023-07-14 18:17:45 +0900

    9610 해결

---
+++ bronze/9610.py
@@ -0,0 +1,20 @@
+n = int(input())
+Q1 = Q2 = Q3 = Q4 = AXIS = 0
+
+for i in range(n):
+    x, y = map(int, input().split())
+    if x == 0 or y == 0:
+        AXIS += 1
+    elif x > 0 and y > 0:
+        Q1 += 1
+    elif x < 0 and y > 0:
+        Q2 += 1
+    elif x < 0 and y < 0:
+        Q3 += 1
+    elif x > 0 and y < 0:
+        Q4 += 1
+print("Q1: {}".format(Q1))
+print("Q2: {}".format(Q2))
+print("Q3: {}".format(Q3))
+print("Q4: {}".format(Q4))
+print("AXIS: {}".format(AXIS))
