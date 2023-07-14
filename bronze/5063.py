b569273b1c9de63555f437420f298f85fae6803b
[KimJungIn] <[rlawjddla0203@gmail.com]>
2023-07-14 16:17:02 +0900

    5063 해결

---
+++ bronze/5063.py
@@ -0,0 +1,9 @@
+N = int(input())
+for i in range(N):
+    r, e, c = map(int, input().split())
+    if r>e-c:
+        print("do not advertise")
+    elif r == e-c:
+        print("does not matter")
+    else:
+        print("advertise")
