5995105f77be2f82b417b85e043b17d752e59b4a
KimJungIn <rlawjddla0203@gmail.com>
2023-07-20 21:10:09 +0900

    11050 해결

--- bronze/11050.py
+++ bronze/11050.py
@@ -1,3 +1,18 @@
 # 이항 계수 구하기
+# dp 알고리즘 이용
+def binomial_coefficient(n, k):
+    if k == 0 or k == n:
+        return 1
+    
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = 1
+    for i in range(1, n + 1):
+        for j in range(1, min(i, k) + 1):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+    
+    return dp[n][k]
 
-N, K = int(input().split())
+n, k = map(int,input().split())
+result = binomial_coefficient(n, k)
+print(result)
