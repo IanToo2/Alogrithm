041765a2a2ab5dcd06f75051e25724360232af69
[KimJungIn] <[rlawjddla0203@gmail.com]>
2023-07-26 11:44:05 +0900

    주석 수정

--- silver/11866.py
+++ silver/11866.py
@@ -1,16 +1,17 @@
+# 빠른 입력을 위한 sys 사용
 import sys
 
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
-# 요세푸스 순열 생성
+# 인덱스에 접근할 변수
 count = 0
 # n의 크기의 리스트 생성
 people = [i for i in range(1, n+1)]
 # 결과를 저장할 리스트
 result = []
 
+# 요세푸스 순열 시작
 while people:
     
     count += k - 1
