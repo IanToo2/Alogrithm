4b6f88f4e36c5d93abe6cd621cc5c95ab1584f6b
[KimJungIn] <[rlawjddla0203@gmail.com]>
2023-09-18 18:09:24 +0900

    재귀 함수 대신 단순 for문으로 수정

--- baekjoon/silver/1676.py
+++ baekjoon/silver/1676.py
@@ -1,28 +1,24 @@
 N = int(input())
 
 def factorial(N):
-    if N == 1:
-        return 1
-    return N * factorial(N-1)
-
+    cal =1
+    for i in range(1,N+1):
+        cal = cal * i
+    return cal
 result = factorial(N)
-print("int result : {}".format(result))
-print("")
 
 str_result = list(reversed(str(result)))
 i = 0   
 count = 0
 
 while True:
-    print("len of str_result :", len(str_result))
-    print("now count size :", count)
-    print("")
+
     if i >= len(str_result):
-        print("count : ",count)
+        print(count)
         break
 
     if str_result[i] != "0":
-        print("count : ",count)
+        print(count)
         break
     else:
         count+=1
