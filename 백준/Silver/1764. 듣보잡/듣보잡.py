fb3ad189bca8046d1fea6cb1b312f29031688cd4
Kim Jungin <rlawjddla0203@gmail.com>
2023-11-01 15:26:37 +0900

    [Silver IV] Title: 듣보잡, Time: 100 ms, Memory: 43412 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,16 +1,17 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-data = set()  # 중복을 허용하지 않는 집합으로 변경
-answer_set = set()  # 중복된 값만 저장하는 집합
+data = set()
+answer_set = set()
 
 for i in range(N + M):
-    check = sys.stdin.readline().strip()  # 줄 끝의 개행 문자 제거
-    if check in data:  # 이미 등장한 문자인지 확인
-        answer_set.add(check)  # 중복된 값만 저장하는 집합에 추가
-    data.add(check)  # 중복을 허용하지 않는 집합에 추가
+    check = sys.stdin.readline().strip()
+    if check in data:
+        answer_set.add(check)
+    else:
+        data.add(check)
 
-answer = sorted(answer_set)  # 중복된 값을 정렬하여 리스트로 변환
+answer = sorted(answer_set)
 
 print(len(answer))
 print(*answer, sep="\n")
