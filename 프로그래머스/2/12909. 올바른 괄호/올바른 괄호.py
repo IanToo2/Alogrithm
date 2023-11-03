cf1a9f19d629e5216874ed1d875530d4b36611f2
Kim Jungin <rlawjddla0203@gmail.com>
2023-11-03 15:30:46 +0900

    [level 2] Title: 올바른 괄호, Time: 39.55 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(s):
+    answer = True
+    total_len = len(s)
+
+    
+    if s[0] == ')':
+        answer = False
+        return answer
+    
+    q= deque()
+    r= deque()
+    
+    for i in s:
+        if i == ')':
+            r.append(i)
+        else:
+            q.append(i)
+        
+        if len(q) < len(r):
+
+            return False
+        
+        if len(r) > total_len/2:
+            return False
+        if len(q) > total_len/2:
+            return False
+
+    if len(r) != len(q):
+        answer = False
+        return answer
+    
+    return True
