863df53e3627dd719d1eaa11473795366d8edde1
Kim Jungin <rlawjddla0203@gmail.com>
2023-11-22 16:03:32 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+cnt = 1
+while cnt <= t:
+    for i in range(t - cnt):
+        print(" ", end='')
+    for i in range(cnt):
+        print("*", end='')
+    print("")
+
+    cnt += 1
+cnt -= 2
+while cnt != 0:
+    for i in range(t-cnt):
+        print(" ", end='')
+    for i in range(cnt):
+        print("*", end='')
+    print("")
+    cnt -= 1
