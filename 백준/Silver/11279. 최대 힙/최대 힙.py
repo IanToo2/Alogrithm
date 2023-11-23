4c3df03fbf189e257018a138bc24cc5566c5a710
Kim Jungin <rlawjddla0203@gmail.com>
2023-11-23 17:45:43 +0900

    [Silver II] Title: 최대 힙, Time: 152 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+input = sys.stdin.readline
+heap =[]
+
+t = int(input())
+
+for i in range(t):
+    input_data = int(input())
+
+    if input_data == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print("0")
+    else:
+        heapq.heappush(heap, -input_data)
