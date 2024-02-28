d41dd387ac95640e3ad6f4d2f6038298c3ab6139
Kim Jungin <rlawjddla0203@gmail.com>
2024-02-28 09:33:57 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 76 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+import org.xml.sax.helpers.AttributeListImpl;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+
+	public static void main(String[] args) throws Exception {
+		init();
+		run();
+	}
+
+	public static void init() throws Exception {
+		System.out.println("       _.-;;-._");
+	    System.out.println("'-..-'|   ||   |");
+	    System.out.println("'-..-'|_.-;;-._|");
+	    System.out.println("'-..-'|   ||   |");
+	    System.out.println("'-..-'|_.-''-._|");
+	}
+
+	public static void run() {
+	}
+}
