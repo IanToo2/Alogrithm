327b2faa266e3610038671661bfc9f41451baf35
Kim Jungin <rlawjddla0203@gmail.com>
2024-03-13 17:30:27 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 924 ms, Memory: 102420 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    // --------------------------------------- //
+    static int N, M, start, cnt;
+    static boolean[] v;
+    static long[] answer;
+    static int count = 2;
+    static ArrayList<TreeSet<Integer>> map = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        init();
+//		print();
+        run();
+    }
+
+    public static void init() throws Exception {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        v = new boolean[N + 1];
+        answer = new long[N + 1];
+
+        M = Integer.parseInt(st.nextToken());
+
+        start = Integer.parseInt(st.nextToken());
+        answer[start] = 0;
+        cnt = 2;
+        v[start] = true;
+
+        for (int i = 0; i <= N; i++) {
+            map.add(new TreeSet<Integer>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            map.get(u).add(v);
+            map.get(v).add(u);
+        }
+
+//		System.out.println(map);
+    }
+
+    public static void run() {
+        dfs(start, 1);
+
+
+        long result = 0 ;
+        for (int i = 1; i <= N; i++) {
+            if (i != start && answer[i] == 0) {
+//				System.out.println(-1);
+            } else {
+//				System.out.println(answer[i]);
+                result += answer[i];
+            }
+//			System.out.println(i + " " + answer[i]);
+        }
+        System.out.println(result);
+    }
+
+    public static void dfs(int cur, int depth) {
+        for (int output : map.get(cur)) {
+            if (v[output]) {
+                continue;
+            }
+//			System.out.println(output);
+            v[output] = true;
+            answer[output] = depth * (long) count;
+            count ++;
+
+            dfs(output, depth + 1);
+        }
+    }
+
+    public static void print() {
+        for (TreeSet<Integer> output : map) {
+            System.out.println(output);
+        }
+    }
+}
