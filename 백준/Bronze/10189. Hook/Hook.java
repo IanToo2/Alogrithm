ce01b7c7de362add8e53c3310b6bb46727b3fd6c
Kim Jungin <rlawjddla0203@gmail.com>
2024-03-16 10:33:25 +0900

    [Bronze V] Title: Hook, Time: 72 ms, Memory: 11272 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    // -----------------------------------------------------------------------------------//
+    static int N, M;
+    static int[] data;
+    public static void main(String[] args) throws Exception {
+        init();
+    }
+
+    public static void init() throws Exception{
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+
+
+
+
+
+    }
+
+    public static void run() {
+
+    }
+
+    public static void dfs(int depth) {
+
+    }
+}
