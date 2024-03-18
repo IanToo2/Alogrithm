cc8a17152a0014ab06f05b903c54ba1b135e183e
Kim Jungin <rlawjddla0203@gmail.com>
2024-03-18 10:28:26 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 72 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    // --------------------------------------- //
+
+    public static void main(String[] args) throws Exception {
+        init();
+        run();
+    }
+
+    public static void init() throws Exception {
+
+    }
+
+    public static void run() {
+        System.out.println("    8888888888  888    88888");
+        System.out.println("   88     88   88 88   88  88");
+        System.out.println("    8888  88  88   88  88888");
+        System.out.println("       88 88 888888888 88   88");
+        System.out.println("88888888  88 88     88 88    888888");
+        System.out.println();
+        System.out.println("88  88  88   888    88888    888888");
+        System.out.println("88  88  88  88 88   88  88  88");
+        System.out.println("88 8888 88 88   88  88888    8888");
+        System.out.println(" 888  888 888888888 88  88      88");
+        System.out.println("  88  88  88     88 88   88888888");
+
+
+
+
+
+
+
+    }
+}
