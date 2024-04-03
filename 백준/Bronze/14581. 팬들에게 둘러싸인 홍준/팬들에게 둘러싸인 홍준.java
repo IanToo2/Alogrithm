bb72b2442e190788ed5037274a1a48f92cb4ea62
Kim Jungin <rlawjddla0203@gmail.com>
2024-04-03 09:21:01 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 76 ms, Memory: 11316 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		String input = br.readLine();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + input + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
