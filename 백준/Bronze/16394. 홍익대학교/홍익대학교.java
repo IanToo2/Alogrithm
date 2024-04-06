e7d5d904b39518c28a836d3b71ebf5bdd9be6727
Kim Jungin <rlawjddla0203@gmail.com>
2024-04-06 14:01:14 +0900

    [Bronze V] Title: 홍익대학교, Time: 76 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.print(a-1946);
+    }
+}
