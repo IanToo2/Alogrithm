06f216299d0b514dd2d05c51b73bd215666c7ad1
Kim Jungin <rlawjddla0203@gmail.com>
2024-05-07 16:36:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 118 ms, Memory: 26,604 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -2,40 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 
 	static int n, m, c, answer;
 	static boolean[] v;
-	static int[] personA = new int[2];
-	static int[] dataA = new int[2];
-
-	static int[] personB = new int[2];
-	static int[] dataB = new int[2];
 
 	static int[][] map;
 	static int[][] keep;
 
 	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= t; i++) {
 			init();
-			run();
+			answer = run();
 			sb.append("#").append(i).append(" ").append(answer).append("\n");
 		}
 		System.out.print(sb);
 	}
 
 	private static void init() throws Exception {
-		answer = 0;
-		st = new StringTokenizer(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		n = Integer.parseInt(st.nextToken());
 		map = new int[n][n];
 		keep = new int[n][n];
+		
 		m = Integer.parseInt(st.nextToken());
 		v = new boolean[m];
+		
 		c = Integer.parseInt(st.nextToken());
 
 		for (int i = 0; i < n; i++) {
@@ -44,16 +40,18 @@ public class Solution {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
+		setMap();
 	}
 
-	private static void run() {
-		setMap();
-//		printKeep();
+	private static int run() {
 		int max = 0;
+		// A좌표 고르기
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= n - m; j++) {
-				// set A
+				// B좌표 고르기
 				for (int x = i; x < n; x++) {
+					// 같은 행에서는 A좌표의 y값보다 더 크게
 					if (x == i) {
 						for (int y = j + m; y <= n - m; y++) {
 							max = Math.max(max, keep[i][j] + keep[x][y]);
@@ -66,7 +64,7 @@ public class Solution {
 				}
 			}
 		}
-		answer = max;
+		return max;
 	}
 
 	private static void recur(int[] person, int[] data, int cost, int nowC) {
@@ -74,6 +72,8 @@ public class Solution {
 		if (nowC > c) {
 			return;
 		}
+		
+		// 값이 최대 인 경우
 		if (cost > data[0]) {
 			data[0] = cost;
 			data[1] = nowC;
@@ -89,6 +89,7 @@ public class Solution {
 		}
 	}
 
+	// 각 좌표별 최대값을 keep에 미리 저장하는 함수
 	private static void setMap() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= n - m; j++) {
@@ -98,15 +99,6 @@ public class Solution {
 				recur(now, data, 0, 0);
 				keep[i][j] = data[0];
 			}
-		}
-	}
-
-	private static void printKeep() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				System.out.print(keep[i][j] + " ");
-			}
-			System.out.println();
 		}
 	}
 }
