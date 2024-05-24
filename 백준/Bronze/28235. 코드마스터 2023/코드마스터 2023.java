be2df07168324356cd0f9b1fcf2e50df2fa1457d
Kim Jungin <rlawjddla0203@gmail.com>
2024-05-25 00:44:37 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 104 ms, Memory: 12764 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+
+		if (input.equals("SONGDO")) {
+			System.out.println("HIGHSCHOOL");
+		} else if (input.equals("CODE")) {
+			System.out.println("MASTER");
+		} else if (input.equals("2023")) {
+			System.out.println("0611");
+		} else if (input.equals("ALGORITHM")) {
+			System.out.println("CONTEST");
+		}
+	}
+}
