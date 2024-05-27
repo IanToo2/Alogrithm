048556c3f080fee121145f46fea294337367e6a4
Kim Jungin <rlawjddla0203@gmail.com>
2024-05-27 12:02:51 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[(num_list.length-1) / n + 1];
+        for(int i = 0 ; i < (num_list.length-1)/n + 1;i++){
+            answer[i] = num_list[n*i];
+        }
+        return answer;
+    }
+}
