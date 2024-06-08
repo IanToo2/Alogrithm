e4a588ef2243b935b207c31e6208553e4b4be57a
Kim Jungin <rlawjddla0203@gmail.com>
2024-06-08 20:01:14 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -6,11 +6,10 @@ public class Solution {
         
         int cost = 0;
         
-        while(n!=0){
-            if(n%2 == 0){
-                n = n/2;
-            }else{
-                n-=1;
+        String binaryN = Integer.toBinaryString(n);
+        
+        for(int i = 0 ; i < binaryN.length() ; i++){
+            if(binaryN.charAt(i) == '1') {
                 cost++;
             }
         }
