20477a5109cb7ee201ab7f4f55ba5cfb50b6749a
Kim Jungin <rlawjddla0203@gmail.com>
2024-06-10 14:19:33 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 1;
+        
+        if(my_string.length() < is_prefix.length()){
+            return 0;
+        }
+        
+        for(int i = 0 ; i < is_prefix.length(); i++){
+            if(my_string.charAt(i) != is_prefix.charAt(i)){
+                answer = 0;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
