20ac133425b17523cfb690f1f95ab16f748d9b35
Kim Jungin <rlawjddla0203@gmail.com>
2024-06-17 21:29:10 +0900

    [level 2] Title: 튜플, Time: 3.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public Set solution(String s) {
         Set<Integer> set = new LinkedHashSet<>();
-        // String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
         s = s.substring(2, s.length()-2);
 
         s = s.replace("},{",  "-");
@@ -11,17 +10,13 @@ class Solution {
 
         Arrays.sort(arr, Comparator.comparingInt(String::length));
         StringTokenizer st;
-        // System.out.println("set add...");
         for(String data : arr){
             st = new StringTokenizer(data, ",");
             while(st.hasMoreTokens()){
                 String value = st.nextToken();
-                // System.out.println(value);
                 set.add(Integer.parseInt(value));
             }
         }
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(set);
         return set;
     }
 }
