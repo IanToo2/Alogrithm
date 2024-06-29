a91265c138e9b421972707eca3132f91f741a94a
Kim Jungin <rlawjddla0203@gmail.com>
2024-06-30 01:21:37 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 135 ms, Memory: 18,676 KB -BaekjoonHub

---
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Node[] tree;
+    static class Node{
+        char value;
+        Node left;
+        Node right;
+    }
+
+    public static void main(String[] args) throws Exception{
+        for(int i = 1; i <= 10; i++){
+            init();
+            System.out.print("#"+i+" ");
+            inorder(tree[1]);
+            System.out.println();
+        }
+    }
+    private static void init() throws Exception{
+        StringTokenizer st;
+
+        int size = Integer.parseInt(br.readLine());
+
+        tree = new Node[size+1];
+        for(int i = 1; i <=size;i++){
+            tree[i] = new Node();
+        }
+
+        // root => always num 1
+        for(int i = 0; i < size ; i++){
+            st = new StringTokenizer(br.readLine());
+            int nodeNum = Integer.parseInt(st.nextToken());
+            // node value
+            tree[nodeNum].value = (st.nextToken()).charAt(0);
+            // left child
+            if(st.hasMoreTokens()){
+                int leftNode = Integer.parseInt(st.nextToken());
+                tree[nodeNum].left = tree[leftNode];
+            }
+            // right child
+            if(st.hasMoreTokens()){
+                int rightNode = Integer.parseInt(st.nextToken());
+                tree[nodeNum].right = tree[rightNode];
+            }
+        }
+    }
+    private static void inorder(Node cur){
+        if(cur.left != null) inorder(cur.left);
+        System.out.print(cur.value);
+        if(cur.right != null) inorder(cur.right);
+    }
+}
