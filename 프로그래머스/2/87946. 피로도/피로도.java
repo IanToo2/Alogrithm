013784c92342cfeee1e5138ecf6d4d3eabee1d92
Kim Jungin <rlawjddla0203@gmail.com>
2024-07-01 09:38:21 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    static int size;
+    static boolean[] v;
+    static int answer = -1;
+    
+    public int solution(int k, int[][] dungeons) {
+        size = dungeons.length;
+        
+        v = new boolean[size+1];
+        
+        recur(k, dungeons, 0);
+        int returnAnswer = answer;
+        return returnAnswer;
+    }
+    private static void recur(int k, int[][] dungenons, int depth){
+        // System.out.println(k + " , " + depth);
+        answer = Math.max(depth, answer);
+        
+        for(int i = 0 ; i < size ; i++){
+            if(v[i]){
+                continue;
+            }
+            if(k >= dungenons[i][0]){
+                v[i] = true;
+                recur(k - dungenons[i][1], dungenons, depth+1);
+                v[i] = false;
+            }
+        }
+    }
+}
