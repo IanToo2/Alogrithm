7f4026b8c1617375cb5bc6c058a76071129fcac0
Kim Jungin <rlawjddla0203@gmail.com>
2024-07-01 09:44:04 +0900

    [level 3] Title: 야근 지수, Time: 111.99 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int work : works){
+            
+            q.add(work);
+        }
+        while(n-- > 0){
+            int work = q.poll();
+            if(work == 0){
+                break;
+            }
+            q.add(work-1);
+        }
+        
+        while(!q.isEmpty()){
+            int work = q.poll();
+            answer += work*work;
+        }
+        return answer;
+    }
+}
