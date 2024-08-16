155ab3c3f57450187364631cc5ae44d9593a8917
Kim Jungin <rlawjddla0203@gmail.com>
2024-08-16 16:39:55 +0900

    [Silver I] Title: 그림, Time: 296 ms, Memory: 44704 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,79 +1,87 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static int n, m;
-    static int maxValue = 0;
-    static int[][] map;
+    static int n, m, picCount, maxSize;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
     static boolean[][] v;
-    static Deque<int[]> q = new ArrayDeque<>();
 
     public static void main(String[] args) throws Exception {
-        init();
-        run();
-    }
-
-    public static void init() throws Exception {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        picCount = 0;
+        maxSize = 0;
         map = new int[n][m];
         v = new boolean[n][m];
+
+        // input value
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                // 입력을 받으며 일부 방문처리
+                if (map[i][j] == 0) {
+                    v[i][j] = true;
+                }
             }
         }
+
+        logic();
+        System.out.println(picCount);
+        System.out.println(maxSize);
+//        printMap(map);
     }
 
-    public static void run() {
-        int cnt = 0;
+    private static void logic() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (v[i][j] || map[i][j] == 0) {
-                    continue;
+                // start bfs
+                if (!v[i][j]) {
+                    picCount++;
+                    bfs(i, j);
                 }
-//                System.out.println(cnt + "번째 시작" + i + "," + j);
-                cnt++;
-                bfs(i, j);
             }
         }
-//        System.out.println("cnt : " + cnt);
-//        System.out.println("maxValue : " + maxValue);
-        System.out.println(cnt);
-        System.out.println(maxValue);
     }
 
-    public static void bfs(int x, int y) {
+    private static void bfs(int x, int y) {
+        Deque<int[]> q = new ArrayDeque<>();
         v[x][y] = true;
+
         q.add(new int[]{x, y});
         int size = 0;
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             size++;
-
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || v[nx][ny]) continue;
-
-                if (map[nx][ny] == 1) {
-                    v[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || v[nx][ny]) {
+                    continue;
                 }
+
+                v[nx][ny] = true;
+                q.add(new int[]{nx, ny});
+
             }
         }
-        if (size > maxValue) {
-            maxValue = size;
-        }
-//        System.out.println(size);
+
+        maxSize = Math.max(size, maxSize);
     }
+
+    private static void printMap(int[][] map) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
