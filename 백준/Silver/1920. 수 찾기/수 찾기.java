edcec255607ed8a6f6053d9df321f5828269f771
Kim JungIn <rlawjddla0203@gmail.com>
2025-04-01 14:54:42 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 63600 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,54 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static int n;
-	static List<Long> list;
-
 	public static void main(String[] args) throws Exception {
-		init();
-		int t = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < t; i++) {
-			long target = Long.parseLong(st.nextToken());
+		HashSet<Long> set = new HashSet<>();
 
-			int mid;
-			int low = 0;
-			int high = n - 1;
-			boolean flag = false;
-			while (low <= high) {
-
-				mid = (low + high) / 2;
-
-				if (list.get(mid) == target) {
-					flag = true;
-					sb.append("1").append("\n");
-					break;
-				} else if (target < list.get(mid)) {
-					high = mid - 1;
-				} else {
-					low = mid + 1;
-				}
-			}
-			if (!flag) {
-				sb.append("0").append("\n");
-			}
-		}
-		System.out.println(sb.toString());
-	}
-
-	private static void init() throws IOException {
-		n = Integer.parseInt(br.readLine());
-		list = new ArrayList<Long>();
-		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			list.add(Long.parseLong(st.nextToken()));
+			set.add(Long.parseLong(st.nextToken()));
 		}
-		Collections.sort(list);
+
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			int before = set.size();
+			Long data = Long.parseLong(st.nextToken());
+			set.add(data);
+			int after = set.size();
+			if (before == after) {
+				sb.append("1");
+			} else {
+				set.remove(data);
+				sb.append("0");
+			}
+
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
 	}
 }
