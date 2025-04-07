f9c7eb6b25777b98912682abaa84d3c8646c780d
Kim JungIn <rlawjddla0203@gmail.com>
2025-04-07 22:17:21 +0900

    [Silver III] Title: 파일 정리, Time: 536 ms, Memory: 82364 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -8,21 +8,14 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		int n = Integer.parseInt(br.readLine());
-		HashMap<String, Integer> map = new HashMap<>();
+		TreeMap<String, Integer> map = new TreeMap<>();
 		for (int i = 0; i < n; i++) {
-			String input = br.readLine();
-			st = new StringTokenizer(input, ".");
+			st = new StringTokenizer(br.readLine(), ".");
 			st.nextToken();
 			String keep = st.nextToken();
 			map.put(keep, map.getOrDefault(keep, 0) + 1);
 		}
-
-		List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-		list.sort((a, b) -> {
-			return a.getKey().compareTo(b.getKey());
-		});
-
-		for (Map.Entry<String, Integer> cur : list) {
+		for(Map.Entry<String, Integer> cur : map.entrySet()) {
 			sb.append(cur.getKey()).append(" ").append(cur.getValue()).append("\n");
 		}
 		System.out.println(sb);
